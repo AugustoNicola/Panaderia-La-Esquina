fabb284f2b6d453bea09439496fd65e19b3e1820
lambda <augusp64@gmail.com>
2021-04-01 00:27:07 -0300

    :electric_plug: Conexión básica de servidor y BBDD

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const mongoose = require("mongoose")
+
+// ==================== middleware ====================
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+// ==================== conexion a la BBDD ====================
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}, err => {
+	if(err) throw err;
+	console.log("Conectado a la BBDD de MongoDB!")
+})
+
+// ==================== inicializacion de servidor ====================
+app.get("/", (req, res) => {
+	res.json({msg: "Funcionando!"})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+	console.log("Servidor iniciado en el puerto ", PORT)
+})
