1511b97ada6eadd66947382f7033ed55114ed483
lambda <augusp64@gmail.com>
2021-04-15 01:26:45 -0300

    :newspaper: :iphone: :computer: Estilos Footer

--- client/src/Componentes/Utilidades/Footer/Footer.jsx
+++ client/src/Componentes/Utilidades/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import "./Footer.css";
+
 const Footer = () => {
 	return (
-		<div>
-			
+		<>
+		<div className="footer">
+			<h3 className="logo-footer">La Esquina</h3>
+			<nav className="navegacion-footer">
+				<a href="">Página Principal</a>
+				<a href="">Tienda Virtual</a>
+				<a href="">Contacto</a>
+			</nav>
+			<div className="redes-sociales">
+				<i class="fab fa-facebook-f"></i>
+				<i class="fab fa-twitter"></i>
+				<i class="fab fa-instagram"></i>
+			</div>
 		</div>
+		<div className="copyright">
+			<p>Copyright &copy; 2021 Augusto Nicola</p>
+		</div>
+		</>
 	)
 };
 
