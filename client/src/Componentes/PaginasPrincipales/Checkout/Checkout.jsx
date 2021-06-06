07f3d0e3ec81fe8248faefaf899d85a91817a11c
lambda <augusp64@gmail.com>
2021-06-06 13:31:34 -0300

    :newspaper: :electric_plug: Conexión con API de PayPal

--- client/src/Componentes/PaginasPrincipales/Checkout/Checkout.jsx
+++ client/src/Componentes/PaginasPrincipales/Checkout/Checkout.jsx
@@ -1,4 +1,5 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import {PayPalScriptProvider, PayPalButtons, FUNDING} from "@paypal/react-paypal-js";
 
 import "./Checkout.css";
 import { EstadoGlobal } from "../../../EstadoGlobal";
@@ -8,6 +9,15 @@ const Checkout = () => {
 	const estado = useContext(EstadoGlobal);
 	const carrito = estado.usuarioAPI.carrito[0];
 	const [mensajeError, setMensajeError] = useState("");
+	
+	const [habilitarPaypal, setHabilitarPaypal] = useState(true);
+	const opcionesPaypal = {
+		"client-id": "AXHJZrXdVMOSroBaaqIZzgKXtL914XZm8s7zvtPWBWW5l-3ICKzotmKtpqm1_NOtLd36puXeIKBulvrJ",
+		"buyer-country": "AR",
+		locale: "es_AR",
+		currency: "USD",
+		intent: "capture"
+	};
 	
 	const [credenciales, setCredenciales] = useState({
 		nombre: "",
@@ -29,74 +39,94 @@ const Checkout = () => {
 	};
 	
 	return (
-		<main className="seccion">
-							
-			{(estado.usuarioAPI.sesionIniciada[0] && carrito[0]) && //* solo se muestra si hay sesion y el carrito tiene elementos
-			<form onSubmit={cambioInput} className="contenedor-facturacion">
-				<div className="formulario-facturacion" data-transicion style={{animationDelay: "0.4s"}}>
-					<h1>Detalles de Facturación</h1>
+		<PayPalScriptProvider options={opcionesPaypal}>
+			<main className="seccion">
+								
+				{(estado.usuarioAPI.sesionIniciada[0] && carrito[0]) && //* solo se muestra si hay sesion y el carrito tiene elementos
+				<form onSubmit={cambioInput} className="contenedor-facturacion">
+					<div className="formulario-facturacion" data-transicion style={{animationDelay: "0.4s"}}>
+						<h1>Detalles de Facturación</h1>
+						
+						<MensajeError mensaje={mensajeError} /> 
+						
+						<input type="text" name="nombre" required autoComplete="on" placeholder="Nombre" value={credenciales.nombre} onChange={cambioInput} className="campo" />
+						
+						<input type="text" name="apellido" required autoComplete="on" placeholder="Apellido" value={credenciales.apellido} onChange={cambioInput} className="campo" />
+						
+						<input type="text" name="direccion" required autoComplete="on" placeholder="Dirección (calle y número)" value={credenciales.direccion} onChange={cambioInput} className="campo" />
+						
+						<input type="text" name="extraDireccion" autoComplete="on" placeholder="Depto, etc (opcional)" value={credenciales.extraDireccion} onChange={cambioInput} className="campo" />
+						
+						<input type="email" name="email" required autoComplete="on" placeholder="Correo Electrónico" value={credenciales.email} onChange={cambioInput} className="campo" />
+						
+						<input type="number" name="telefono" required autoComplete="on" value={credenciales.telefono} placeholder="Teléfono" min="1000000000" max="9999999999" onChange={cambioInputTelefono} className="campo" />
+						
+					</div>
 					
-					<MensajeError mensaje={mensajeError} /> 
-					
-					<input type="text" name="nombre" required autoComplete="on" placeholder="Nombre" value={credenciales.nombre} onChange={cambioInput} className="campo" />
-					
-					<input type="text" name="apellido" required autoComplete="on" placeholder="Apellido" value={credenciales.apellido} onChange={cambioInput} className="campo" />
-					
-					<input type="text" name="direccion" required autoComplete="on" placeholder="Dirección (calle y número)" value={credenciales.direccion} onChange={cambioInput} className="campo" />
-					
-					<input type="text" name="extraDireccion" autoComplete="on" placeholder="Depto, etc (opcional)" value={credenciales.extraDireccion} onChange={cambioInput} className="campo" />
-					
-					<input type="email" name="email" required autoComplete="on" placeholder="Correo Electrónico" value={credenciales.email} onChange={cambioInput} className="campo" />
-					
-					<input type="number" name="telefono" required autoComplete="on" value={credenciales.telefono} placeholder="Teléfono" min="1000000000" max="9999999999" onChange={cambioInputTelefono} className="campo" />
-					
-				</div>
-				
-				<div className="pedido-facturacion" data-transicion style={{animationDelay: "0.6s"}}>
-					<h1>Tu Pedido</h1>
-					<div className="productos">	
-						{
-						carrito.map((producto) => {
-							return (
-								<div className="producto">
-									<h3>{producto.nombre}</h3>
-									<p className="cantidad">
-										<span>{producto.cantidad}x </span>
-										${producto.precio}
-									</p>
-									<p className="subtotal">${producto.cantidad * producto.precio}</p>
-								</div>
-							)
-						})
-						}
-						<div className="total">
-							<p className="etiqueta-total">Total Final</p>
-							<p className="precio-total">${carrito.reduce((previo, producto) => {return previo + (producto.precio * producto.cantidad)},0)}</p>
+					<div className="pedido-facturacion" data-transicion style={{animationDelay: "0.6s"}}>
+						<h1>Tu Pedido</h1>
+						<div className="productos">	
+							{
+							carrito.map((producto) => {
+								return (
+									<div className="producto" key={producto._id}>
+										<h3>{producto.nombre}</h3>
+										<p className="cantidad">
+											<span>{producto.cantidad}x </span>
+											${producto.precio}
+										</p>
+										<p className="subtotal">${producto.cantidad * producto.precio}</p>
+									</div>
+								)
+							})
+							}
+							<div className="total">
+								<p className="etiqueta-total">Total Final</p>
+								<p className="precio-total">${carrito.reduce((previo, producto) => {return previo + (producto.precio * producto.cantidad)},0)}</p>
+							</div>
+						</div>
+						<div className="boton-paypal-contenedor">	
+							<PayPalButtons 
+								disabled={!habilitarPaypal}
+								style = {{
+									color: "gold",
+									tagline: false,
+									height: 55
+								}}
+								fundingSource={FUNDING.PAYPAL}
+								createOrder = {(data, actions) => {
+									return actions.order.create({
+										purchase_units: [{
+											amount: {
+												value: '1.23'
+											}
+										}]
+									});
+								}}
+							/>
 						</div>
 					</div>
 					
-					<button type="submit" className="boton" data-transicion style={{animationDelay: "0.8s"}}>Realizar Pedido</button>
-				</div>
+				</form>
+				}
 				
-			</form>
-			}
-			
-			{(estado.usuarioAPI.sesionIniciada[0] && !carrito[0]) && //? usuario sin carrito
-			<div className="operacion-invalida" data-transicion style={{animationDelay: "0.2s"}}>
-				<h2>¡Ups!</h2>
-				<h3>Tu carrito está vacío</h3>
-				<h4>Agregá tus productos favoritos para realizar un pedido.</h4>
-			</div>
-			}
-			
-			{!estado.usuarioAPI.sesionIniciada[0] && //? no hay sesion 
-			<div className="operacion-invalida" data-transicion style={{animationDelay: "0.2s"}}>
-				<h2>¡Ups!</h2>
-				<h3>No iniciaste sesión con un usuario</h3>
-				<h4>Iniciá sesión para realizar un pedido.</h4>
-			</div>
-			}
-		</main>
+				{(estado.usuarioAPI.sesionIniciada[0] && !carrito[0]) && //? usuario sin carrito
+				<div className="operacion-invalida" data-transicion style={{animationDelay: "0.2s"}}>
+					<h2>¡Ups!</h2>
+					<h3>Tu carrito está vacío</h3>
+					<h4>Agregá tus productos favoritos para realizar un pedido.</h4>
+				</div>
+				}
+				
+				{!estado.usuarioAPI.sesionIniciada[0] && //? no hay sesion 
+				<div className="operacion-invalida" data-transicion style={{animationDelay: "0.2s"}}>
+					<h2>¡Ups!</h2>
+					<h3>No iniciaste sesión con un usuario</h3>
+					<h4>Iniciá sesión para realizar un pedido.</h4>
+				</div>
+				}
+			</main>
+		</PayPalScriptProvider>
 	)
 };
 
