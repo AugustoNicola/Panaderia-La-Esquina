59ab33e9849dd5aa7bbc6de0a8d6d18563f9b87e
lambda <augusp64@gmail.com>
2021-06-12 19:57:04 -0300

    :newspaper: :wrench: :computer: Producto: sin scroll

--- client/src/Componentes/PaginasPrincipales/Producto/Producto.jsx
+++ client/src/Componentes/PaginasPrincipales/Producto/Producto.jsx
@@ -32,7 +32,7 @@ const Producto = () => {
 			{
 				//* busca productos relacionados
 				const categoriaABuscar = productoSeleccionado.categorias ? productoSeleccionado.categorias[0] : "";
-				const respuesta = await axios.get(`http://localhost:5000/api/productos?categoria=${categoriaABuscar}&busqueda=&orden=-updatedAt&pagina=1&limite=6`);
+				const respuesta = await axios.get(`http://localhost:5000/api/productos?categoria=${categoriaABuscar}&busqueda=&orden=-updatedAt&pagina=1&limite=4`);
 				if(respuesta.status === 200) setProductosRelacionados(respuesta.data.productos);
 			}
 		};
