0907fb64f99331b499e6480ffa203a39ddd81515
lambda <augusp64@gmail.com>
2021-06-13 22:36:03 -0300

    :newspaper: :wrench: Restaurar Scroll al cambiar de página

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import "./normalize.css";
 import "./App.css";
+import RestaurarScroll from "./Componentes/Utilidades/RestaurarScroll/RestaurarScroll";
 import PaginaInicio from "./Componentes/PaginasPrincipales/PaginaInicio/PaginaInicio";
 import Header from "./Componentes/Utilidades/Header/Header";
 import Footer from "./Componentes/Utilidades/Footer/Footer";
@@ -13,16 +14,17 @@ const App = () => {
 	return(
 		<ProveedorEstado>
 			<Router>
+				<RestaurarScroll />
 				<div className="app">
 					<Switch>
 						<Route exact path="/" component={PaginaInicio} />
-
+						
 						<Route path="*">	
 							<Header />
 							<EnrutadorPaginaPrincipal />
 						</Route>
 					</Switch>
-
+					
 					<Footer />
 				</div>
 			</Router>
